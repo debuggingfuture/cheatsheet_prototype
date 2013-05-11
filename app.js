fea5aa8328d99cd73f1ca46443a6818b2e5e6463
lcy <lauchunyin@gmail.com>
2013-05-11 22:30:30 +0800

    ask west for help on editor

--- app.js
+++ app.js
@@ -6,18 +6,24 @@
  var express = require('express')
  , routes = require('./routes')
  , user = require('./routes/user')
+  , editor = require('./routes/editor')
  , http = require('http')
  , path = require('path');
 
  var app = express();
  var hbs = require('hbs');
 
+ var fs = require('fs');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
+
+//use .html extension
 app.set('view engine', 'html');
 app.engine('html', require('hbs').__express);
+
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -29,9 +35,9 @@ app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// res.render('users', {
-//     output: 'from handlebars'
-//   })
+
+//think about the case for client-side
+hbs.registerPartial('footer', fs.readFileSync(__dirname + '/views/footer.html', 'utf8'));
 
 
 // development only
@@ -39,8 +45,20 @@ if ('development' == app.get('env')) {
 	app.use(express.errorHandler());
 }
 
+app.get('/template', function(req,res){
+	res.render('template',{
+		output:'123'
+	});
+
+})
+
+
+
+app.get('/editor', editor.editor);
+
 app.get('/', routes.index);
 app.get('/users', user.list);
+
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
